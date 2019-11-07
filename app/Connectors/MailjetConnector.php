0680c246a99051e4efe1f62b2ca3a6bfdc843bf6
Fellipe <fellipecapelli@gmail.com>
2019-11-07 07:07:19 -0300

    Add email connectors

---
+++ app/Connectors/MailjetConnector.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Connectors;
+
+use Mailjet\Client;
+
+final class MailjetConnector implements Connector
+{
+    private $client;
+
+    public function __construct (array $config)
+    {
+        $this->client = new Client(
+            $config['key'],
+            $config['secret'],
+            $config['performer'],
+            [
+                'version' => $config['version']
+            ]
+        );
+    }
+
+    public function getClient (): Client
+    {
+        return $this->client;
+    }
+}
