94b83b9685e901efe419017e807da5b4f189ba24
Fellipe <fellipecapelli@gmail.com>
2019-11-09 15:43:35 -0300

    Update code style on email controller

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreEmailPost;
+use App\Transactors\MailjetTransactor;
 use Illuminate\Http\JsonResponse;
 
 class EmailController extends Controller
@@ -12,10 +13,17 @@ class EmailController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  StoreEmailPost  $request
+     * @param  MailjetTransactor  $transactor
      * @return JsonResponse
      */
-    public function store(StoreEmailPost $request): JsonResponse
-    {
+    public function store(
+        StoreEmailPost $request,
+        MailjetTransactor $transactor
+    ): JsonResponse {
+        $payload = $transactor->preparePayload($request->all());
+
+        $payload->send();
+
         return response()->json($request->all());
     }
 }
