29029d22e6a8a363fccc52efb5ce2bc30b08b1b6
Fellipe <fellipecapelli@gmail.com>
2019-11-10 22:15:11 -0300

    Update mailjet transactor

--- app/Transactors/MailjetTransactor.php
+++ app/Transactors/MailjetTransactor.php
@@ -5,59 +5,68 @@ namespace App\Transactors;
 
 use Mailjet\Resources;
 use App\Connectors\MailjetConnector;
-use App\Enum\MailjetMessages;
+use App\Enum\Email;
+use App\Enum\MailjetEmail;
 
 final class MailjetTransactor implements Transactor
 {
     private $client;
-    private $messageBody = [ MailjetMessages::MESSAGES_KEY ];
+    private $messageBody = [];
 
     public function __construct(MailjetConnector $maijetConnector)
     {
         $this->client = $maijetConnector->getClient();
     }
 
-    public function preparePayload(array $inputData): self
+    public function preparePayload(array $inputData): array
     {
+        $this->messageBody[MailjetEmail::MESSAGES_KEY_MAILJET] = [];
+
         foreach ($inputData as $key => $value) {
-            if ($key === MailjetMessages::FROM_KEY) {
-                $this->messageBody[MailjetMessages::MESSAGES_KEY][] = $this->prepareFromPayload($value);
+            if ($key === Email::FROM_KEY) {
+                $this->messageBody[MailjetEmail::MESSAGES_KEY_MAILJET][0][MailjetEmail::FROM_KEY_MAILJET] =
+                    $this->prepareFromPayload($value);
+                continue;
             }
 
-            if ($key === MailjetMessages::TO_KEY) {
-                $this->messageBody[MailjetMessages::MESSAGES_KEY][] = $this->prepareToPayload($value);
+            if ($key === Email::TO_KEY) {
+                $this->messageBody[MailjetEmail::MESSAGES_KEY_MAILJET][0][MailjetEmail::TO_KEY_MAILJET] =
+                    $this->prepareToPayload($value);
+                continue;
             }
 
-            if ($key === MailjetMessages::SUBJECT_KEY) {
-                $this->messageBody[MailjetMessages::MESSAGES_KEY][MailjetMessages::SUBJECT_KEY_MAILJET] = $value;
+            if ($key === Email::SUBJECT_KEY) {
+                $this->messageBody[MailjetEmail::MESSAGES_KEY_MAILJET][0][MailjetEmail::SUBJECT_KEY_MAILJET] = $value;
+                continue;
             }
 
-            $this->messageBody[MailjetMessages::MESSAGES_KEY][] = $key === MailjetMessages::TEXTPART_KEY
-                ? [MailjetMessages::TEXTPART_KEY_MAILJET => $value ]
-                : [MailjetMessages::HTMLPART_KEY_MAILJET => $value ];
+            if ($key === Email::TEXTPART_KEY) {
+                $this->messageBody[MailjetEmail::MESSAGES_KEY_MAILJET][0][MailjetEmail::TEXTPART_KEY_MAILJET] = $value;
+                continue;
+            }
+
+            $this->messageBody[MailjetEmail::MESSAGES_KEY_MAILJET][0][MailjetEmail::HTMLPART_KEY_MAILJET] = $value;
         }
 
-        return $this;
+        return $this->messageBody;
     }
 
     private function prepareFromPayload(array $userData): array
     {
         return [
-            MailjetMessages::FROM_KEY_MAILJET => [
-                MailjetMessages::EMAIL_KEY_MAILJET  => $userData[MailjetMessages::EMAIL_KEY],
-                MailjetMessages::NAME_KEY_MAILJET   => $userData[MailjetMessages::NAME_KEY]
-            ]
+            MailjetEmail::EMAIL_KEY_MAILJET  => $userData[Email::EMAIL_KEY],
+            MailjetEmail::NAME_KEY_MAILJET   => $userData[Email::NAME_KEY]
         ];
     }
 
     private function prepareToPayload(array $userData): array
     {
-        $userPayload = [ MailjetMessages::TO_KEY_MAILJET ];
+        $userPayload = [];
 
         foreach ($userData as $value) {
-            $userPayload[MailjetMessages::TO_KEY_MAILJET][] = [
-                MailjetMessages::EMAIL_KEY_MAILJET  => $value[MailjetMessages::EMAIL_KEY],
-                MailjetMessages::NAME_KEY_MAILJET   => $value[MailjetMessages::NAME_KEY]
+            $userPayload[] = [
+                MailjetEmail::EMAIL_KEY_MAILJET  => $value[Email::EMAIL_KEY],
+                MailjetEmail::NAME_KEY_MAILJET   => $value[Email::NAME_KEY]
             ];
         }
 
