a7f3b714cbebe3ffd415c771a7778d81b2fcc3e3
Fellipe <fellipecapelli@gmail.com>
2019-11-10 22:12:49 -0300

    Create Enum for email definitions only, without keeping definitions used when using vendors, such as mailjet

---
+++ app/Enum/Email.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Enum;
+
+class Email
+{
+    public const FROM_KEY = 'from';
+    public const TO_KEY = 'to';
+    public const EMAIL_KEY = 'email';
+    public const NAME_KEY = 'name';
+    public const SUBJECT_KEY = 'subject';
+    public const TEXTPART_KEY = 'textPart';
+    public const HTMLPART_KEY = 'htmlPart';
+}
