9cfb45412426107a10092b99168f346baa815fa1
Fellipe <fellipecapelli@gmail.com>
2019-11-11 23:51:35 -0300

    Add default const for default config on email publisher

--- app/Publishers/EmailPublisher.php
+++ app/Publishers/EmailPublisher.php
@@ -5,14 +5,15 @@ namespace App\Publishers;
 final class EmailPublisher
 {
     private const DEFAULT_RETRY = 0;
+    private const DEFAULT_CONFIG = 'default';
 
     private $queueName;
     private $routingKeyName;
 
     public function __construct()
     {
-        $this->queueName = env('RABBITMQ_QUEUE', 'default');
-        $this->routingKeyName = env('RABBITMQ_ROUTING_KEY_NAME', 'default');
+        $this->queueName = env('RABBITMQ_QUEUE', self::DEFAULT_CONFIG);
+        $this->routingKeyName = env('RABBITMQ_ROUTING_KEY_NAME', self::DEFAULT_CONFIG);
     }
 
     /**
