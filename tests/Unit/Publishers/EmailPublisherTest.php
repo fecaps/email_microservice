d953d279927d650e4141e1016364ea6571759714
Fellipe <fellipecapelli@gmail.com>
2019-11-11 23:51:01 -0300

    Add publishers tests

---
+++ tests/Unit/Publishers/EmailPublisherTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit\Publishers;
+
+use Tests\TestCase;
+use App\Publishers\EmailPublisher;
+
+class EmailPublisherTest extends TestCase
+{
+    private $publisher;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->publisher = new EmailPublisher();
+    }
+
+    /**
+     * Email Publisher test
+     *
+     * @dataProvider \Tests\Unit\DataProviders\ValidEmailsDataProvider::emails()
+     * @param array  $email
+     * @return void
+     */
+    public function testEmailTransactions(array $email) {
+        $this->publisher->handle($email);
+        $this->assertInstanceOf(EmailPublisher::class, $this->publisher);
+    }
+}
