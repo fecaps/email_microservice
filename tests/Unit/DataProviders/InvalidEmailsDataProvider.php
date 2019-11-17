f0bb87a7d8f16ee881ac02a758a02f7c3dc76757
Fellipe <fellipecapelli@gmail.com>
2019-11-17 10:17:48 -0300

    Add markdown part on data providers used by unit tests

--- tests/Unit/DataProviders/InvalidEmailsDataProvider.php
+++ tests/Unit/DataProviders/InvalidEmailsDataProvider.php
@@ -21,7 +21,6 @@ final class InvalidEmailsDataProvider
                     ],
                     'subject' => 'Mailjet Transactor 1',
                     'textPart' => 'Hello, text part 1',
-                    'htmlPart' => 'Hello, HTML part 1',
                 ]
             ],
             [
@@ -38,10 +37,26 @@ final class InvalidEmailsDataProvider
                         ]
                     ],
                     'subject' => 'Mailjet Transactor 2',
-                    'textPart' => 'Hello, text part 2',
                     'htmlPart' => 'Hello, HTML part 2',
                 ]
-            ]
+            ],
+            [
+
+                [
+                    'from' => [
+                        'email' => 'fellipe',
+                        'name' => 'Fellipe Capelli'
+                    ],
+                    'to' => [
+                        [
+                            'email' => 'fellipe.capelli@outlook.com',
+                            'name' => 'Fellipe C. Fregoneze'
+                        ]
+                    ],
+                    'subject' => 'Mailjet Transactor 3',
+                    'markdownPart' => 'Hello, markdown part 3',
+                ]
+            ],
         ];
     }
 }
--- tests/Unit/DataProviders/ValidEmailsDataProvider.php
+++ tests/Unit/DataProviders/ValidEmailsDataProvider.php
@@ -11,17 +11,16 @@ final class ValidEmailsDataProvider
                 [
                     'from' => [
                         'email' => 'fellipecapelli@gmail.com',
-                        'name' => 'Fellipe Capelli'
+                        'name' => 'Fellipe Capelli',
                     ],
                     'to' => [
                         [
                             'email' => 'fellipe.capelli@outlook.com',
-                            'name' => 'Fellipe C. Fregoneze'
+                            'name' => 'Fellipe C. Fregoneze',
                         ]
                     ],
                     'subject' => 'Mailjet Transactor 1',
                     'textPart' => 'Hello, text part 1',
-                    'htmlPart' => 'Hello, HTML part 1',
                 ]
             ],
             [
@@ -38,10 +37,26 @@ final class ValidEmailsDataProvider
                         ]
                     ],
                     'subject' => 'Mailjet Transactor 2',
-                    'textPart' => 'Hello, text part 2',
                     'htmlPart' => 'Hello, HTML part 2',
                 ]
-            ]
+            ],
+            [
+
+                [
+                    'from' => [
+                        'email' => 'fellipecapelli@gmail.com',
+                        'name' => 'Fellipe Capelli'
+                    ],
+                    'to' => [
+                        [
+                            'email' => 'fellipe.capelli@outlook.com',
+                            'name' => 'Fellipe C. Fregoneze'
+                        ]
+                    ],
+                    'subject' => 'Mailjet Transactor 3',
+                    'markdownPart' => 'Hello, markdown part 3',
+                ]
+            ],
         ];
     }
 }
