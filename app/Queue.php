aac010be485c699bb2c667dd6ae17fbdc5912155
Fellipe <fellipecapelli@gmail.com>
2019-11-17 22:03:24 -0300

    Add methods to update status on queue model

--- app/Queue.php
+++ app/Queue.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Queue extends Model
 {
-    //
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function addToQueue(array $data): int
+    {
+        $data = self::create([
+            'status' => 'queued',
+            'message' => json_encode([ 'data' => $data ]),
+        ]);
+
+        return $data['id'];
+    }
+
+    public function updateStatusToBounced(int $id): void
+    {
+        self::where([ 'id' => $id ])
+            ->update([ 'status' => 'bounced' ]);
+    }
+
+    public function updateStatusToFailed(int $id): void
+    {
+        self::where([ 'id' => $id ])
+            ->update([ 'status' => 'failed' ]);
+    }
+
+    public function updateStatusToDelivered(int $id): void
+    {
+        self::where([ 'id' => $id ])
+            ->update([ 'status' => 'delivered' ]);
+    }
 }
