1942fb2123333806d7b38a03a8eb197381370d98
Fellipe <fellipecapelli@gmail.com>
2019-11-17 22:02:41 -0300

    Add usage of enum for log messages and queue model on controllers

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreEmailPost;
 use App\Publishers\EmailPublisher;
+use App\Queue;
+use App\Enum\LogMessages;
 use Illuminate\Http\JsonResponse;
 
 class EmailController extends Controller
@@ -12,19 +14,26 @@ class EmailController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  StoreEmailPost  $request
-     * @param  EmailPublisher  $publisher
+     * @param StoreEmailPost  $request
+     * @param EmailPublisher  $publisher
+     * @param Queue  $queue
      * @return JsonResponse
      */
     public function store(
         StoreEmailPost $request,
-        EmailPublisher $publisher
+        EmailPublisher $publisher,
+        Queue $queue
     ): JsonResponse {
-        \Log::channel('publisher')->info('New message requested - Web');
-        $publisher->handle($request->all());
+        \Log::channel('publisher')->info(LogMessages::START_WEB);
+
+        $data = $request->all();
+        $id = $queue->addToQueue($data);
+        $fullData = array_merge($data, [ 'id' => $id ]);
+
+        $publisher->handle($fullData);
 
         return response()
-            ->json([ 'data' => $request->all()])
+            ->json([ 'data' => $fullData ])
             ->setStatusCode(201);
     }
 }
